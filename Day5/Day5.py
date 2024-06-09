d196e61446bba6cab767cddd0294104e7d056030
MadaKusuma <a710230022@student.ums.ac.id>
2024-06-09 11:55:23 +0700

    Create Day5.py

---
+++ Day5/Day5.py
@@ -0,0 +1,9 @@
+nama = input('Siapa nama kamu : ')
+
+umur = input ('Berapa umur kamu: ')
+
+alamat = input ('Dimana alamatmu : ')
+
+print('hallo ', nama)
+print('umur kamu ', umur)
+print('alamat kamu ', alamat)

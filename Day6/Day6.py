18514d48763340343a0d0d890664c9c5348493ce
MadaKusuma <a710230022@student.ums.ac.id>
2024-06-10 11:44:47 +0700

    Create Day6.py

---
+++ Day6/Day6.py
@@ -0,0 +1,10 @@
+print("Menghitung Luas dan Keliling Persegi")
+
+panjang = input('Masukkan nilai Panjang: ')
+
+lebar = input ('Masukkan nilai Lebar: ')
+
+luas = int(panjang) * int(lebar)
+keliling = 2 * (int(panjang) + int(lebar))
+
+print ('Luas Persegi adalah ', luas ,' sedangkan kelilingnya adalah ',keliling)

b267619e5570cce74d86a4f06c58044343f61ed7
MadaKusuma <a710230022@student.ums.ac.id>
2024-06-14 00:53:14 +0700

    Create Day10.py

---
+++ Day10/Day10.py
@@ -0,0 +1,9 @@
+angka = float(input('Masukkan angka :'))
+
+if angka > 0:
+    if angka == 0:
+        print('Angka nol')
+    else:
+        print('Angka Positif')
+else:
+    print('Angka Negatif')

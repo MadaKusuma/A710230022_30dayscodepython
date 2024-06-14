829349179d21da34e764c535335eb373a1c929ce
MadaKusuma <a710230022@student.ums.ac.id>
2024-06-15 00:10:37 +0700

    Create Day11.py

---
+++ Day11/Day11.py
@@ -0,0 +1,10 @@
+nilai = 60
+
+if nilai > 90:
+    print('Nilai A')
+if nilai >= 75 and nilai <= 89:
+    print('Nilai B')
+if nilai >= 60 and nilai <= 74:
+    print('nilai C')
+if nilai >= 45 and nilai <= 59:
+    print('nilai D') 
